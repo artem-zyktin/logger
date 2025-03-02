45c727c2edcd6ebc63f5ae17cbad87766b2d9a62
a.zyktin <zyktin.a.a@gmail.com>
2025-03-02 15:57:38 +0800

    Add makefile and shortcut

---
+++ make_project.bat
@@ -0,0 +1,1 @@
+﻿"./tools/premake/bin/win/premake5.exe" --file="makefiles/premake5.lua" vs2022
---
+++ makefiles/premake5.lua
@@ -0,0 +1,60 @@
+﻿rootdir  = '../'
+builddir = rootdir .. 'build/'
+bindir   = rootdir .. 'bin/'
+src      = rootdir .. 'src/'
+
+configdir       = bindir .. '%{cfg.architecture}/%{cfg.buildcfg}/'
+intermadiatedir = configdir .. 'intermediate/'
+libdir          = configdir .. 'lib/'
+outputdir       = configdir .. 'output/'
+
+workspace 'Logger'
+	configurations { 'Debug', 'Release' }
+	architecture 'x64'
+	location '../build/%{_ACTION}'
+
+project 'logger'
+	kind 'StaticLib'
+	language 'C++'
+	cppdialect 'C++20'
+	targetdir (libdir)
+	objdir (intermadiatedir)
+
+	logger_srcdir = src .. 'logger/'
+
+	files {
+		logger_srcdir .. '**.h',
+		logger_srcdir .. '**.hpp',
+		logger_srcdir .. '**.cpp',
+	}
+
+	filter 'configurations:Debug'
+		defines { '_DEBUG' }
+		symbols 'On'
+
+	filter 'configurations:Release'
+		defines { 'NDEBUG' }
+		optimize 'On'
+
+project 'logger_test'
+	kind 'ConsoleApp'
+	language 'C++'
+	cppdialect 'C++20'
+	targetdir (outputdir)
+	objdir (intermadiatedir)
+
+	test_srcdir = src .. 'test/'
+
+	files {
+		test_srcdir .. '**.h',
+		test_srcdir .. '**.hpp',
+		test_srcdir .. '**.cpp'
+	}
+
+	filter 'configurations:Debug'
+		defines { '_DEBUG' }
+		symbols 'On'
+
+	filter 'configurations:Release'
+		defines { 'NDEBUG' }
+		optimize 'On'
