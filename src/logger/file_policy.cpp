ad8bdf46fa53497c5c94972247b2274189b0a1c6
a.zyktin <zyktin.a.a@gmail.com>
2025-03-09 01:03:58 +0800

    Default file policy refactoring accroding to Releaseable policy concept

--- src/logger/file_policy.cpp
+++ src/logger/file_policy.cpp
@@ -8,12 +8,18 @@ std::mutex DefaultFileLoggerPolicy::log_file_mutex_;
 
 void DefaultFileLoggerPolicy::set_file_path(std::string_view file_path)
 {
+	release();
+
+	std::scoped_lock lock(log_file_mutex_);
+	log_file_.open(std::string(file_path), std::ios::out | std::ios::app);
+}
+
+void DefaultFileLoggerPolicy::release()
+{
 	std::scoped_lock lock(log_file_mutex_);
 
 	if (log_file_.is_open())
 		log_file_.close();
-
-	log_file_.open(std::string(file_path), std::ios::out | std::ios::app);
 }
 
 void DefaultFileLoggerPolicy::write(std::string_view message)
