23dfd754856e616290ab70381be96b7646444068
a.zyktin <zyktin.a.a@gmail.com>
2025-03-16 00:35:57 +0800

    Add logger_test.cpp source for tests

---
+++ src/test/logger_test.cpp
@@ -0,0 +1,24 @@
+﻿#include "logger/logger.hpp"
+#include "logger/default_console_policy.hpp"
+
+#include <gtest/gtest.h>
+
+namespace logger_test
+{
+
+TEST(LoggerTest, BasicLogging)
+{
+	using logger_t = logger::Logger<logger::DefaultConsoleLoggerPolicy>;
+
+	logger_t log ();
+
+	EXPECT_TRUE(true);
+}
+
+}
+
+int main(int argc, char* argv[])
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
