f7f1e2515549bd09a7b73cc209b66aa862dc5f95
a.zyktin <zyktin.a.a@gmail.com>
2025-03-30 09:39:55 +0800

    Add validating failure test

--- src/test/logger_test.cpp
+++ src/test/logger_test.cpp
@@ -140,6 +140,27 @@ TEST(LoggerTest, ConfigParsingValidatingSuccess)
 	EXPECT_TRUE(logger::validate_config(config));
 }
 
+TEST(LoggerTest, ConfigParsingValidatingFailure)
+{
+	constexpr std::string_view log_file = "log.txt";
+	constexpr std::string_view log_pattern = "[{{level}}][{{time}}][{{thread-id}] {{message}}";
+
+	constexpr std::string_view json_config = R"(
+	{{
+		"logger" : {{
+			"log_file": "{}",
+			"log_level": "inf",
+			"log_pattern": "{}"
+		}}
+	}})";
+
+	std::string json_text = std::format(json_config, log_file, log_pattern);
+
+	logger::LoggerConfig config = logger::read_config_from_json(json_text);
+
+	EXPECT_FALSE(logger::validate_config(config));
+}
+
 }
 
 int main(int argc, char* argv[])
