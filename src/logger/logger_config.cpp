a3cdd1f3a97fb2acda4f658b150a367eb8cd357e
a.zyktin <zyktin.a.a@gmail.com>
2025-03-30 08:56:35 +0800

    Add implementation of validate_config function

--- src/logger/logger_config.cpp
+++ src/logger/logger_config.cpp
@@ -6,6 +6,9 @@
 #include <rapidjson/error/en.h>
 
 #include <iostream>
+#include <functional>
+#include <array>
+#include <format>
 
 namespace fs = std::filesystem;
 
@@ -35,28 +38,66 @@ std::string parse_log_file(Value const * const logger_section)
 	return result;
 }
 
+std::string parse_config_str(Value const * const section, std::string_view member_name, std::string_view default_value = "")
+{
+	if (section->HasMember(member_name.data()) && (*section)[member_name.data()].IsString())
+		return (*section)["log_pattern"].GetString();
+
+	return std::string(default_value);
+}
+
 Level parse_log_level(Value const * const logger_section)
 {
-	Level result = DEFAULT_LOG_LEVEL;
-	if (logger_section->HasMember("log_level"))
-	{
-		if ((*logger_section)["log_level"].IsString())
-			result = str_to_level((*logger_section)["log_level"].GetString());
-	}
-
-	return result;
+	return str_to_level(parse_config_str(logger_section, "log_level", "debug"));
 }
 
 std::string parse_log_pattern(Value const * const logger_section)
 {
-	std::string result = std::string(DEFAULT_LOG_PATTERN);
-	if (logger_section->HasMember("log_pattern"))
+	return parse_config_str(logger_section, "log_pattern", DEFAULT_LOG_PATTERN);
+}
+
+inline void replace_placeholder(std::string& pattern, std::string_view placeholder, std::string_view value)
+{
+	size_t index = 0;
+	while ((index = pattern.find(placeholder, index)) != std::string::npos)
 	{
-		if ((*logger_section)["log_pattern"].IsString())
-			result = std::string((*logger_section)["log_pattern"].GetString());
+		pattern.replace(index, placeholder.size(), value);
+		index += value.size();
+	}
+}
+
+inline void replace_log_pattern_placeholders(std::string& pattern)
+{
+	using value_t = std::pair<std::string_view, std::string_view>;
+
+	static constexpr std::array<value_t, 4> variables = { {
+		{ "{{time}}",      "{1}" },
+		{ "{{thread-id}}", "{2}" },
+		{ "{{level}}",     "{3}" } ,
+		{ "{{message}}",   "{4}" }
+	} };
+
+	std::ranges::for_each(variables, [&pattern](const value_t& item) mutable
+	{
+		replace_placeholder(pattern, item.first, item.second);
+	});
+}
+
+bool validate_config_log_pattern(const LoggerConfig& config)
+{
+	std::string log_pattern = copy(config.log_pattern);
+	replace_log_pattern_placeholders(log_pattern);
+
+	try
+	{
+		std::vformat(log_pattern, std::make_format_args("1"sv, "2"sv, "3"sv, "4"sv));
+	}
+	catch (const std::format_error&)
+	{
+		return false;
 	}
 
-	return result;
+	return true;
 }
 
 } // namespace
@@ -106,6 +147,23 @@ LoggerConfig read_config_from_json(const std::string& json_text)
 	config.log_pattern = parse_log_pattern(logger_section);
 
 	return config;
+}
+
+bool validate_config(const LoggerConfig& config)
+{
+	using validate_func_t = std::function<bool(const LoggerConfig&)>;
+	static std::array<validate_func_t, 1> validators = {
+		validate_config_log_pattern,
+	};
+
+	bool result = true;
+	(void) std::ranges::find_if_not(validators, [&result, &config](const validate_func_t& validator) mutable
+	{
+		result = validator(config);
+		return result;
+	});
+
+	return result;
 }
 
 }// namespace logger
