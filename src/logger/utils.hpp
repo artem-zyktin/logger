7e672cdbd7691e9d1d652f481eb032959b6f1630
a.zyktin <zyktin.a.a@gmail.com>
2025-03-30 08:55:19 +0800

    Add explicit copy function

--- src/logger/utils.hpp
+++ src/logger/utils.hpp
@@ -5,4 +5,12 @@
 namespace logger
 {
 	std::string read_file(const std::filesystem::path& filepath);
+	
+	template<class T>
+		requires std::is_copy_constructible_v<std::remove_cvref_t<T>>
+	constexpr std::remove_cvref_t<T> copy(const T& value)
+	{
+		return value;
+	}
+
 } // namespace logger
